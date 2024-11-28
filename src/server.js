117484f7a3221fd1a2d33d54580e95259c843b96
Anderson Coelho <anderson.coelho@indt.org.br>
2024-11-28 18:39:03 -0400

    create methods GET/POST to handle registers on database volatile

--- src/server.js
+++ src/server.js
@@ -1,7 +1,40 @@
 import http from 'node:http'
+import { randomUUID } from 'node:crypto';
+import { json } from './middlewares/json.js'
+import { Database } from './database.js';
 
-const server = http.createServer((req, res) => {
-    return res.end('Hello World');
+const database = new Database
+
+const server = http.createServer(async (req, res) => {
+    const { method, url } = req;
+
+    await json(req, res);
+
+    if (method === 'GET' && url === '/tasks') {
+        const tasks = database.select('tasks');
+
+        return res.end(JSON.stringify(tasks));
+    }
+    
+    if (method === 'POST' && url === '/tasks') {
+        const { title, description } = req.body;
+
+        const task = {
+            id: randomUUID(),
+            title,
+            description,
+            completed: false,
+            completed_at: null,
+            created_at: new Date().toISOString(),
+            updated_at: null,
+        }
+
+        database.insert('tasks', task);
+
+        return res.writeHead(201).end();
+    }
+
+    return res.writeHead(404).end();
 });
 
 server.listen(3334);
